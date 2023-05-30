90c5cba1cf7c9718ba30a36577cd201e25397789
ninoxander <za220111414@zapopan.tecmm.edu.mx>
2023-05-29 19:01:44 -0600

    Añadiendo las operaciones basicas

--- src/Operator.java
+++ src/Operator.java
@@ -1,6 +1,24 @@
 package src;
 
 public class Operator {
+ /*
+ Implementar suma, resta, multiplicacion y division.
+  */   
+    public static double suma(double a, double b){
+        return a+b;
+    }
+    public static double resta(double a, double b){
+        return a-b;
     
-    
+    }
+    public static double multiplicacion(double a, double b){
+        return a*b;
+    }
+    public static double division(double a, double b){
+        if( a==0 || b == 0){
+            return -1;
+        }
+        return a/b;
+    }
+
 }
